81441845066dc2b7919edb38fa58b802eb85a739
E11iZzz <elizarov-2015@bk.ru>
2024-12-06 11:41:51 +0300

    1.0
    uploaded main files

---
+++ Temp/Includes/Globalvar.h
@@ -0,0 +1,39 @@
+/* Automation Studio generated header file */
+/* Do not edit ! */
+
+#ifndef _BUR_1575039658_1_
+#define _BUR_1575039658_1_
+
+#include <bur/plctypes.h>
+
+/* Constants */
+#ifdef _REPLACE_CONST
+#else
+#endif
+
+
+/* Variables */
+_GLOBAL plcbit gAxis_X_iNegHwEnd;
+_GLOBAL plcbit gAxis_X_iPosHwEnd;
+_GLOBAL struct SdcEncIf16_typ gAxis_X_EncIf;
+_GLOBAL struct SdcDiDoIf_typ gAxis_X_DiDoIf;
+_GLOBAL struct SdcDrvIf16_typ gAxis_X_DrvIf;
+_GLOBAL struct SdcHwCfg_typ gAxis_X_HW;
+_GLOBAL struct ACP10AXIS_typ gAxis_X;
+
+
+
+
+
+__asm__(".section \".plc\"");
+
+/* Used IEC files */
+__asm__(".ascii \"iecfile \\\"Logical/Global.var\\\" scope \\\"global\\\"\\n\"");
+
+/* Exported library functions and function blocks */
+
+__asm__(".previous");
+
+
+#endif /* _BUR_1575039658_1_ */
+
